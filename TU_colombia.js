6f1e5ef1d09f9cfcadcded220f0516c9a2381c5a
Wei Lai <wlai@liveperson.com>
2022-10-14 11:37:15 -0400

    15 minutes timeout

--- TU_colombia.js
+++ TU_colombia.js
@@ -1,26 +1,34 @@
 var endFlag = false;
 var timeoutIndex;
-// in milliseconds, 1 second = 1000
-var minuteTimeout = 6000;
-var fiftenMinutesTimeout = 10000;
-var bufferTimeout = 2000;
+// in milliseconds, 1 second = 1000, 1 min = 1sec * 60, 15 min = 1 min * 15
+var secondTimeout = 1000;
+var minuteTimeout = secondTimeout * 60;
+var fiftenMinutesTimeout = minuteTimeout * 15;
+var bufferTimeout = secondTimeout * 2;
+var debug = false;
+
+function tuLog(s) {
+  if (debug) {
+    console.log(new Date().toString() + " : " + s);
+  }
+}
 
 function closeWindow() {
-  console.log("close window");
+  tuLog("close window");
   if ($(".lpc_maximized-header__close-button").length > 0) {
     $(".lpc_maximized-header__close-button").click();
   }
 }
 
 function closeSi() {
-  console.log("close si");
+  tuLog("close si");
   if ($(".lpc_confirmation-dialog__confirm-button").length > 0) {
     $(".lpc_confirmation-dialog__confirm-button").click();
   }
 }
 
 function closeSequence() {
-  console.log("timeout index win " + timeoutIndex);
+  tuLog("timeout index win " + timeoutIndex);
   // close window by x button
   closeWindow();
   // delay close si
@@ -33,17 +41,17 @@ function autoClose(data, eventInfo) {
   // if conversation ended in any way, agent close, customer close, autoclose
   if (data.state == "ended") {
     endFlag = true;
-    console.log("settimeout to close window");
+    tuLog("settimeout to close window");
     // give user a minute before closes UI elements
     timeoutIndex = setTimeout(closeSequence, minuteTimeout);
-    console.log("timeout index is " + timeoutIndex);
+    tuLog("timeout index is " + timeoutIndex);
   } else {
     if (!endFlag) {
       // any other action then reset timer and start a new one to autoclose window
-      console.log("clear previous timeout " + timeoutIndex);
+      tuLog("clear previous timeout " + timeoutIndex);
       clearTimeout(timeoutIndex);
       timeoutIndex = setTimeout(closeSequence, fiftenMinutesTimeout);
-      console.log("new timeout index " + timeoutIndex);
+      tuLog("new timeout index " + timeoutIndex);
     }
   }
 }
