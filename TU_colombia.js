86497d73a9230d273421e16674803230986ba4e4
Wei Lai <wlai@liveperson.com>
2022-10-14 09:20:14 -0400

    refactor

--- TU_colombia.js
+++ TU_colombia.js
@@ -1,12 +1,25 @@
+var endFlag = false;
+var timeoutIndex;
+var minuteTimeout = "6000";
+
+function closeWindow() {
+  console.log("close window");
+  if ($(".lpc_maximized-header__close-button").length > 0) {
+    $(".lpc_maximized-header__close-button").click();
+  }
+}
+
 function autoClose(data, eventInfo) {
   // if conversation ended in any way, agent close, customer close, autoclose
   if (data.state == "ended") {
+    endFlag = true;
+    console.log("settimeout to close window");
     // give user a minute before closes UI elements
-    setTimeout(() => {
-      if ($(".lpc_maximized-header__close-button").length > 0) {
-        $(".lpc_maximized-header__close-button").click();
-      }
-    }, "60000");
+    setTimeout(closeWindow, minuteTimeout);
+  } else {
+    if (!endFlag) {
+      // any other action then reset timer and start a new one to autoclose window
+    }
   }
 }
 lpTag.events.bind("lpUnifiedWindow", "state", autoClose);
