e70080d2ac0153a6ce2356968db290343dd23ca9
Wei Lai <wlai@liveperson.com>
2023-01-13 16:22:18 -0500

    receiver logs post payload

--- webhook-receiver/app.js
+++ webhook-receiver/app.js
@@ -1,9 +1,35 @@
-const http = require('http');
+const fs = require('fs');
 
+const content = 'Some content!';
+
+fs.writeFile('webhooklog.txt', content, err => {
+  if (err) {
+    console.error(err);
+  }
+  // file written successfully
+});
+
+const http = require('http');
 const hostname = '127.0.0.1';
 const port = 8282;
 
 const server = http.createServer((req, res) => {
+  // write req to log file
+  req.on('data', chunk => {
+    console.log(`Data chunk available: ${chunk}`)
+
+    // append to log file
+    fs.appendFile('webhooklog.txt', chunk, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+
+
+  })
+  req.on('end', () => {
+    //end of data
+  })
+  
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
