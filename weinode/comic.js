30cbb5a3cd0060fdbb11fcd5b3dd202121f03b3b
Wei Lai <wlai@liveperson.com>
2023-12-21 13:06:36 -0500

    working download single book

--- weinode/comic.js
+++ weinode/comic.js
@@ -1,34 +1,90 @@
-const axios = require('axios');
-const fs = require('fs');
+const axios = require("axios");
+const fs = require("fs");
 
-// Replace 'https://example.com/api/data' with your API endpoint
-const apiUrl = 'https://6367ec75edc85dbc84debec0.mockapi.io/api/v1/sde';
-const outputFilePath = 'response.jpg';
+const firstPage = 1;
+// const lastPage = 219;
+let currentPage = 17;
+const lastPage = 25;
+const bookNumber = 100;
 
+let hasError = false;
 
-let config = {
-  method: 'get',
-  maxBodyLength: Infinity,
-  responseType: 'arraybuffer',
-  url: 'https://eu1.hamreus.com/ps1/h/op/vol_100/002.jpg.webp',
-  headers: { 
-    'Referer': 'https://tw.manhuagui.com/'
+let page = "";
+async function getNextPage() {
+  // Replace 'https://example.com/api/data' with your API endpoint
+  const apiUrl = "https://6367ec75edc85dbc84debec0.mockapi.io/api/v1/sde";
+  if (currentPage < 10) {
+    page = `00${currentPage}`;
+  } else if (currentPage > 10 && currentPage < 100) {
+    page = `0${currentPage}`;
+  } else {
+    page = currentPage;
   }
-};
+  const outputFilePath = `./files/${bookNumber}-${page}.jpg`;
 
-axios.request(config)
-.then(response => {
-    // Write the binary data to a file
-    fs.writeFile(outputFilePath, response.data, 'binary', (err) => {
-      if (err) {
-        console.error('Error writing to file:', err);
-      } else {
-        console.log('Image saved to', outputFilePath);
-      }
+  let _url = `https://eu1.hamreus.com/ps1/h/op/vol_${bookNumber}/${page}.jpg.webp`;
+
+  // url: 'https://eu1.hamreus.com/ps1/h/op/vol_100/002.jpg.webp',
+
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    responseType: "arraybuffer",
+    url: _url,
+    headers: {
+      Referer: "https://tw.manhuagui.com/",
+    },
+  };
+
+  axios
+    .request(config)
+    .then((response) => {
+      // Write the binary data to a file
+      fs.writeFile(outputFilePath, response.data, "binary", (err) => {
+        if (err) {
+          console.error("Error writing to file:", err);
+        } else {
+          console.log("Image saved to", outputFilePath);
+        }
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      hasError = true;
     });
-  })
-.catch((error) => {
-  console.log(error);
-});
+}
 
+async function delayedLoop() {
+  // Define the condition for the while loop
+  while (currentPage <= lastPage && !hasError) {
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    const delayMilliseconds = randomNumber * 1000;
+    console.log('wait ' + delayMilliseconds);
+    // get page
+    await getNextPage();
+    currentPage++;
 
+    // pause for 2 seconds then do it again
+    setTimeout(() => {
+        delayedLoop();
+    }, delayMilliseconds);
+    
+    // Break the loop if needed
+    // if (counter === someCondition) {
+    //   break;
+    // }
+  }
+
+  console.log("Loop finished");
+}
+
+// Start the delayed loop
+delayedLoop();
+
+// while (currentPage <= lastPage && !hasError) {
+//   const randomNumber = Math.floor(Math.random() * 10) + 1;
+//   const delayMilliseconds = randomNumber * 1000;
+//   setTimeout(getNextPage, delayMilliseconds);
+//   currentPage++;
+// }
+// console.log("Waiting for the timeout to complete...");
